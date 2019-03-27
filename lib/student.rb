14a874d2d09083491c30d28e8d14763b2c2d01a5
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-27 13:52:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,10 @@
 class Student
 
-def hello
-  "Hey there! I'm so excited to learn stuff."
-end
+  def hello
+    puts "Hey there! I'm so excited to learn stuff."
+  end
 
-def raise_hand
-  "Pick me!"
-end
-
+  def raise_hand
+    puts "Pick me!"
+  end
 end
